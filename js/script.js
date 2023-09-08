940e1d1b9b2d109ac7401bd03389780d1a2605a0
Idedav <xdavide.correa@gmail.com>
2023-09-08 16:30:54 +0200

    [ADD] message html

--- js/script.js
+++ js/script.js
@@ -1,27 +1,110 @@
+let name = document.getElementById('name')
 let kilometres = document.getElementById('kilometres')
 let age = document.getElementById('age')
 const btnGen = document.getElementById('btn-gen')
 const price = 0.21
 let ticketPrice = kilometres * price
 let message;
+let offer = 'Biglietto Standard'
 
 
 
 btnGen.addEventListener('click', function(){
     kilometres = parseInt(kilometres.value);
     age = parseInt(age.value);
+    name = name.value;
     ticketPrice = kilometres * price
-    message = 'il costo del tuo biglietto è di: ' + ticketPrice + '€'
+    message = `
+    <h2>Il tuo biglietto:</h2>
+    <div class="container d-flex">
+        <div class="row">
+            <div class="col-5">
+                <h3>Nome Passeggero</h3>
+                <span>${name}</span>
+            </div>
+            <div class="col">
+                <h4>Offerta</h4>
+                <span>${offer}</span>
+            </div>
+            <div class="col">
+                <h4>Carrozza</h4>
+                <span>carrozza</span>
+            </div>
+            <div class="col">
+                <h4>Codice CP</h4>
+                <span>codice</span>
+            </div>
+            <div class="col">
+                <h4>Costo Biglietto</h4>
+                <span>${ticketPrice}</span>
+            </div>
+        </div>
+    </div>
+    `
     document.getElementById('output').innerHTML = message
 
     if(age < 18){
         const discountYoung = ticketPrice * 20 / 100;
         ticketPrice -= discountYoung;
-        message = 'il costo del tuo biglietto scontato Young è di: ' + ticketPrice + '€'
+        offer = 'Biglietto  Young'
+        message =`
+        <h2>Il tuo biglietto:</h2>
+        <div class="container d-flex">
+            <div class="row">
+                <div class="col-5">
+                    <h3>Nome Passeggero</h3>
+                    <span>${name}</span>
+                </div>
+                <div class="col">
+                    <h4>Offerta</h4>
+                    <span>${offer}</span>
+                </div>
+                <div class="col">
+                    <h4>Carrozza</h4>
+                    <span>carrozza</span>
+                </div>
+                <div class="col">
+                    <h4>Codice CP</h4>
+                    <span>codice</span>
+                </div>
+                <div class="col">
+                    <h4>Costo Biglietto</h4>
+                    <span>${ticketPrice}</span>
+                </div>
+            </div>
+        </div>
+        `
     }else if(age > 65){
         const discountSilver = ticketPrice * 40 / 100;
         ticketPrice -= discountSilver
-        message = 'il costo del tuo biglietto scontato Silver è di: ' + ticketPrice + '€'
+        offer = 'Biglietto Silver'
+        message =`
+        <h2>Il tuo biglietto:</h2>
+        <div class="container d-flex">
+            <div class="row">
+                <div class="col-5">
+                    <h3>Nome Passeggero</h3>
+                    <span>${name}</span>
+                </div>
+                <div class="col">
+                    <h4>Offerta</h4>
+                    <span>${offer}</span>
+                </div>
+                <div class="col">
+                    <h4>Carrozza</h4>
+                    <span>carrozza</span>
+                </div>
+                <div class="col">
+                    <h4>Codice CP</h4>
+                    <span>codice</span>
+                </div>
+                <div class="col">
+                    <h4>Costo Biglietto</h4>
+                    <span>${ticketPrice}</span>
+                </div>
+            </div>
+        </div>
+        `
     }
 
     document.getElementById('output').innerHTML = message
